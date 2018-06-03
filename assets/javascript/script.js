a638fa94418aba1ec3e90136be7a81d2941984a4
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-02 22:37:37 -0600

    le sigh

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,7 +21,7 @@ var computerAnswer = computerChoice;
 
 
 // computer chooses word from array XX
-// that word is converted into dhases based on length
+// that word is converted into dhases based on length xx
 // the word is also stored so that it wont be used again
 // if the user guesses a letter in the word, the dash(s) turns into the letter
 // if the user guesses a letter not in the word, one life is lost
@@ -33,21 +33,28 @@ dashedWord.push(selectedWord);
 usedWords.push(computerAnswer);
 dashedWord = computerAnswer.split("");
 var dash = "_ "
+console.log(selectedWord);
+console.log(dashedWord);
 
 
 var numberOfDashes = dashedWord.length;
 console.log(numberOfDashes);
 
-function dashesByLength(dash, num){
+function dashesByLength(dash, num) {
     if (num > 0) {
-    return dash.repeat(num);
-}}
+        return dash.repeat(num);
+    }
+}
 
 document.getElementById("current-word").innerHTML = "<p>Current Word:   </p>" + "<p>" + dashesByLength(dash, numberOfDashes) + "</p>";
 
 
 
-
+function gameReset() {
+    dashedWord = [];
+    selectedWord = [];
+    guessedLetters = [];
+}
 
 
 document.onkeyup = function () {
@@ -56,16 +63,36 @@ document.onkeyup = function () {
     console.log(userGuess);
 
 
+    guessedLetters.push(userGuess);
 
     if (!letters.includes(userGuess)) {
         alert("Please selec a letter from the alphabet.");
     } else {
-        guessedLetters.push(userGuess);
-
-     
-      
+        var pos = dashedWord.indexOf(userGuess);
+        // if (pos > -1) {
+        // for (var pos=0; pos < selectedWord.length; pos++){
+        // selectedWord[pos].replace(/_ /g, userGuess);
     }
-    document.querySelector("#wins").innerHTML = "<p>Wins: </p>" + winsVar;
-    document.getElementById("losses").innerHTML = "<p>Losses: </p>" + lossesVar;
-    document.getElementById("lives").innerHTML = "<p>Lives: </p>" + livesVar;
+    // guessedLetters.push(userGuess);
+    for (var i = 0; i < dashedWord.length; i++) {
+        if (userGuess === dashedWord[i]) {
+
+        }
+        console.log()
+    }
+// } else {
+    // guessedLetters.push(userGuess);
+
+    lives--;
+// }
+console.log(pos);
+// }
+
+if (lives == 0) {
+    losses++;
+    gameReset();
+}
+document.querySelector("#wins").innerHTML = "<p>Wins: </p>" + winsVar;
+document.getElementById("losses").innerHTML = "<p>Losses: </p>" + lossesVar;
+document.getElementById("lives").innerHTML = "<p>Lives: </p>" + livesVar;
 }
