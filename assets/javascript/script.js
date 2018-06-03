e5687bc8b0f3e566fab9cc2ca466aba074e0b6fb
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-02 19:21:01 -0600

    some js code working yay!

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,58 +1,71 @@
-// make an array of duck related words
-//make blank array
-//make array for used words
-// computer selects one word randomly
-//if word has been used, pick another word
-// based on length of word, make a blank template wwith _
-// user should have 10 guesses
-// when a letter is guess correctly, the corressponding __ is replaced with the letter
-// when lives is 0, subtract one from losses
-// if a word is guessed corectly, add one to wins
-//reset word after win or loss
-//show word after loss
-
 var wordBank = ["mallard", "crested", "webbed", "bill", "waterproof"];
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
 var usedWords = [];
 var selectedWord = [];
 var guessedLetters = [];
 var dashedWord = [];
+
+
+var onlyDashes = 0;
 var livesVar = 10;
 var winsVar = 0;
 var lossesVar = 0;
+
+//random choice from wordBank array
 var computerChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
 console.log(computerChoice);
-var computerAnswer = computerChoice;
-console.log(computerChoice);
-console.log(wordbank[0]);
 
-document.onkeydown = function () {
+var computerAnswer = computerChoice;
+
+
+// computer chooses word from array XX
+// that word is converted into dhases based on length
+// the word is also stored so that it wont be used again
+// if the user guesses a letter in the word, the dash(s) turns into the letter
+// if the user guesses a letter not in the word, one life is lost
+// when lives == 0, losses goes up
+// when the word is guessed correctly, wins goes up
+
+selectedWord.push(computerAnswer);
+dashedWord.push(selectedWord);
+usedWords.push(computerAnswer);
+dashedWord = computerAnswer.split("");
+var dash = "_ "
+
+
+var numberOfDashes = dashedWord.length;
+console.log(numberOfDashes);
+
+function dashesByLength(dash, num){
+    if (num > 0) {
+    return dash.repeat(num);
+}}
+
+document.getElementById("current-word").innerHTML = "<p>Current Word:   </p>" + "<p>" + dashesByLength(dash, numberOfDashes) + "</p>";
+
+
+
+
+
+
+document.onkeyup = function () {
+    //user input
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     console.log(userGuess);
-    // computerAnswer.push(selectedWord);
-    // console.log(computerAnswer);
-    // console.log(selectedWord).push(usedWords);
-    // dashedWord = selectedWord.indexOf("userGuess");
-    // console.log(userGuess);
-    // console.log(computerAnswer);
 
-    // if (!letter.includes(userGuess)) {
-        // alert("Please selec a letter from the alphabet.");
-    // } else {
-    //     guessedLetters.push(userGuess);
-    //     if (userGuess == selectedWord.indexOf("userGuess")){
-    //     alert("you got a letter")
-    //     }
-    //     if (userGuess !== selectedWord.indexOf("userGuess")) {
-    //         alert("noo")
-    //         lives--;
-    //     }
 
-        
-    
-    // document.getElementById(wins).innerHTML = "Wins: " + winsVar;
-    // document.getElementById(losses).innerHTML = "Losses: " + lossesVar;
-    // document.getElementById(lives).innerHTML = "Lives: " + livesVar;
-    // document.getElementById(letters-guessed).innerHTML = "Letters guessed: " + guessedLetters;
-    // }
+
+    if (!letters.includes(userGuess)) {
+        alert("Please selec a letter from the alphabet.");
+    } else {
+        guessedLetters.push(userGuess);
+
+     
+      
+    }
+    document.querySelector("#wins").innerHTML = "<p>Wins: </p>" + winsVar;
+    document.getElementById("losses").innerHTML = "<p>Losses: </p>" + lossesVar;
+    document.getElementById("lives").innerHTML = "<p>Lives: </p>" + livesVar;
 }
