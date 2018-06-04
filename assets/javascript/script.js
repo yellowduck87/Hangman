11d10d7e2871677bd2cd2644d5320da2924bf32e
Megan Evans <megan.evans.ducky@gmail.com>
2018-06-03 21:54:02 -0600

    added unsucessful code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,7 @@ var usedWords = [];
 var selectedWord = [];
 var guessedLetters = [];
 var dashedWord = [];
+var blankWordString = "";
 
 
 var onlyDashes = 0;
@@ -28,6 +29,11 @@ var computerAnswer = computerChoice;
 // when lives == 0, losses goes up
 // when the word is guessed correctly, wins goes up
 
+
+// make computerAnwser an array
+// for loop through the array comparing each letter with the guess
+// if the user guess is in the word array, then it is replaced in the dashed word
+
 selectedWord.push(computerAnswer);
 dashedWord.push(selectedWord);
 usedWords.push(computerAnswer);
@@ -46,9 +52,10 @@ function dashesByLength(dash, num) {
     }
 }
 
-document.getElementById("current-word").innerHTML = "<p>Current Word:   </p>" + "<p>" + dashesByLength(dash, numberOfDashes) + "</p>";
+blankWordString = dashesByLength(dash, numberOfDashes);
 
-
+document.getElementById("current-word").innerHTML = "<p>Current Word:   </p>" + "<p>" + blankWordString + "</p>";
+console.log(blankWordString);
 
 function gameReset() {
     dashedWord = [];
@@ -71,28 +78,58 @@ document.onkeyup = function () {
         var pos = dashedWord.indexOf(userGuess);
         // if (pos > -1) {
         // for (var pos=0; pos < selectedWord.length; pos++){
-        // selectedWord[pos].replace(/_ /g, userGuess);
     }
     // guessedLetters.push(userGuess);
-    for (var i = 0; i < dashedWord.length; i++) {
-        if (userGuess === dashedWord[i]) {
 
+
+
+    function cycleThroughWord(dashedWord) {
+        for (pos = 0; pos < dashedWord.length; pos++) {
+            if (userGuess === dashedWord[i]) {
+                return pos;
+                // dashedWord[pos] = userGuess;
+                console.log(dashedWord)
+                console.log(pos);
+                //    blankWordString = blankWordString[pos].replace(/_ /g, userGuess);
+                // console.log(blankWordString)
+                // blankWordString.replaceAt(pos, userGuess);
+                console.log(blankWordString);
+
+
+
+
+
+            }
         }
-        console.log()
     }
-// } else {
+
+
+
+
+    function posReplace(pos, userGuess) {
+        if (userGuess != repl) {
+            while (selectedWord.indexOf(userGuess) !== -1) {
+               var newBlankWordString = blankWordString.replace(userGuess, repl);
+            }
+        }
+        return str.toString();
+    }
+    //take the position from cycle trhough word funcion of the letter in the dashedWord array, replace the charat that possition in the blankwordstring then print to the page
+
+
+    // } else {
     // guessedLetters.push(userGuess);
 
-    lives--;
-// }
-console.log(pos);
-// }
+    // lives--;
+    // // }
+    // // console.log(pos);
+    // // }
 
-if (lives == 0) {
-    losses++;
-    gameReset();
-}
-document.querySelector("#wins").innerHTML = "<p>Wins: </p>" + winsVar;
-document.getElementById("losses").innerHTML = "<p>Losses: </p>" + lossesVar;
-document.getElementById("lives").innerHTML = "<p>Lives: </p>" + livesVar;
+    // if (lives == 0) {
+    //     losses++;
+    //     gameReset();
+    // }
+    document.querySelector("#wins").innerHTML = "<p>Wins: </p>" + winsVar;
+    document.getElementById("losses").innerHTML = "<p>Losses: </p>" + lossesVar;
+    document.getElementById("lives").innerHTML = "<p>Lives: </p>" + livesVar;
 }
